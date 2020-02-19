8c25610167734f7bb59557cb10c96e856358959c
Oliver Hine <git@oliy.co.uk>
2020-02-19 22:13:25 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject superlifter "0.1.0-SNAPSHOT"
+(defproject superlifter "0.1.0"
   :description "A (sort of) DataLoader for Clojure/script"
   :url "https://github.com/oliyh/superlifter"
   :license {:name "Eclipse Public License"
