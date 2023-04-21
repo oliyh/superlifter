f30aa66639ffdb383dfa26d9d5a4008fa4aa1c13
Oliver Hine <git@oliy.co.uk>
2023-04-21 14:31:47 +0100

    Upgrading deps

--- example/project.clj
+++ example/project.clj
@@ -3,23 +3,23 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.pedestal/pedestal.service "0.5.7"]
-                 [io.pedestal/pedestal.jetty "0.5.7"]
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.26"]
-                 [org.slf4j/jcl-over-slf4j "1.7.26"]
-                 [org.slf4j/log4j-over-slf4j "1.7.26"]
-                 [com.walmartlabs/lacinia-pedestal "0.13.0-alpha-1"]
-                 [funcool/promesa "4.0.2"]
-                 [superlifter "0.1.3-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [io.pedestal/pedestal.service "0.5.10"]
+                 [io.pedestal/pedestal.jetty "0.5.10"]
+                 [ch.qos.logback/logback-classic "1.4.7" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "2.0.7"]
+                 [org.slf4j/jcl-over-slf4j "2.0.7"]
+                 [org.slf4j/log4j-over-slf4j "2.0.7"]
+                 [com.walmartlabs/lacinia-pedestal "1.1"]
+                 [superlifter "0.1.5-SNAPSHOT"]
+                 [io.aviso/logging "1.0"]]
   :min-lein-version "2.0.0"
   :source-paths ["src" "../src"]
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
   ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "example.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.7"]
-                                  [clj-http "3.10.0"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.10"]
+                                  [clj-http "3.12.3"]]}
              :uberjar {:aot [example.server]}}
   :main ^{:skip-aot true} example.server)
--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[funcool/urania "0.2.0"]
-                 [funcool/promesa "5.1.0"]
-                 [org.clojure/tools.logging "0.6.0"]]
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]
-                                       [org.clojure/clojurescript "1.10.597"]]}
-             :test     {:dependencies [[com.walmartlabs/lacinia-pedestal "0.14.0-alpha-2"]]}
-             :dev      {:dependencies [[binaryage/devtools "1.0.0"]
-                                       [com.bhauman/figwheel-main "0.2.1"]
-                                       [org.clojure/tools.reader "1.2.2"]
-                                       [cider/piggieback "0.4.1"]
+                 [funcool/promesa "10.0.594"]
+                 [org.clojure/tools.logging "1.2.4"]]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]
+                                       [org.clojure/clojurescript "1.11.60"]]}
+             :test     {:dependencies [[com.walmartlabs/lacinia-pedestal "1.1"]]}
+             :dev      {:dependencies [[binaryage/devtools "1.0.7"]
+                                       [com.bhauman/figwheel-main "0.2.18"]
+                                       [org.clojure/tools.reader "1.3.6"]
+                                       [cider/piggieback "0.5.2"]
                                        [org.clojure/tools.nrepl "0.2.13"]]
                         :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
